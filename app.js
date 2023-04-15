c9831bae34fd1c9bfdadfae5a2f588c75a44438a
SergCastle <117464640+SergCastle@users.noreply.github.com>
2023-04-15 18:49:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
-
 let buy = document.getElementById("buy");
 let order = document.getElementById("order");
 
 buy.addEventListener("click", function(){
     document.getElementById("main").style.display = "none";
     document.getElementById("form").style.display = "block";
-    document.getElementById("user_name").style.display = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
-    });
+});
 
 order.addEventListener("click", function(){
     tg.close():
-    });
+});
